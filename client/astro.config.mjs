b38669c0b2c13192807037d7c38f0056f334467d
Marat200118 <marat.samigullin1@gmail.com>
2023-12-12 17:00:28 +0100

    relations displayed

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -1,15 +1,15 @@
 import { defineConfig } from "astro/config";
 
-// export default defineConfig({
-//   site: "https://marats-samigullins.com/",
-//   base: "/creative-conference/",
-//   image: {
-//     domains: ["localhost"],
-//   },
-// });
-
 export default defineConfig({
+  site: "https://marats-samigullins.com/",
+  base: "Space-Travel-Conference",
   image: {
     domains: ["localhost"],
   },
 });
+
+// export default defineConfig({
+//   image: {
+//     domains: ["localhost"],
+//   },
+// });
